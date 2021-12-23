a6af8de67bc5bc86abe31c5682e551ede6234743
Ramdas Hedgapure <hedgapurer@gmail.com>
2021-12-23 19:11:47 +0530

    webpack babel

--- webpack-react/src/index.js
+++ webpack-react/src/index.js
@@ -1,14 +1,28 @@
 import { add, mult } from "./calc.js";
 import("./index.css")
+import React from "react"
+import ReactDom from "react-dom"
 
 // var a= 4;
 // var b= 4;
 
-console.log( add(2, 3))
-console.log( mult(1, 3))
+// console.log( add(2, 3))
+// console.log( mult(1, 3))
 
-const h1 = document.createElement('h1');
-h1.innerText = "Hello webpack";
-h1.classList.add("redcolor")
+// const h1 = document.createElement('h1');
+// h1.innerText = "Hello webpack";
+// h1.classList.add("redcolor")
 
-document.getElementById("root").appendChild(h1)
+// document.getElementById("root").appendChild(h1)
+
+ReactDom.render(
+   
+    React.createElement("h1", {className : "redcolor"}, "Hello world"),
+    document.getElementById("root")
+)
+
+ReactDom.render(
+   
+    React.createElement("i", {className : "redcolor"}, "Hello world"),
+    document.getElementById("h1")
+)
