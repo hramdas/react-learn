239120465f0e98f1f861dbe2ec7d3991589d25bf
Ramdas Hedgapure <hedgapurer@gmail.com>
2021-12-28 17:11:29 +0530

    form

--- useRef-Forms-event/src/components/Form.jsx
+++ useRef-Forms-event/src/components/Form.jsx
@@ -1,6 +1,6 @@
-import {useState} from 'react'
+import {useState, useRef} from 'react'
 
-// useRef save data if Rerendered
+// form inputs
 
 function Form(){
     const [form, setForm] = useState({
@@ -8,11 +8,14 @@ function Form(){
         age : ""
     })
 
+    const ref = useRef(null)  //for file upload
+
     const handleChange = (e) =>{
         const {name, value} = e.target
         setForm({
             ...form, [name] : value
         })
+        console.log(ref)
     }
 
     const handlesubmit = (e)=>{
@@ -23,6 +26,8 @@ function Form(){
         <form onSubmit={handlesubmit}>
         <input onChange={handleChange} name="username" type="text" placeholder='Enter name' />
         <input onChange={handleChange}  name="age" type="number" placeholder='Enter number' />
+
+        <inpu onChange={handleChange} ref="ref" type="file" />
 
         <input type="submit" />
         
