b808aaa77e13c1ddf4b98e9a643308154b748d1a
Ramdas Hedgapure <hedgapurer@gmail.com>
2021-12-30 00:08:58 +0530

    todo useeffect

---
+++ 4-1React Forms/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+// import Todo from './components/todo/Todo'
+// import Events from './components/Event'
+// import UseRef from './components/UseRef'
+import Form from './components/UserDetail/Form'
+
+
+
+
+function App() {
+ return(
+   <div className='App'> 
+
+    < Form />
+   </div>
+ )
+}
+
+export default App;
---
+++ 4-1React Forms/src/components/UserDetail/List.jsx
@@ -0,0 +1,17 @@
+
+ function List(){
+    // const formList = await fetch("http://localhost:3001/forms")
+    // const data = await formList.json()
+    // console.log(data)
+    return(
+        <div>
+            {/* {data.map((e)=>(
+                // console.log(e.name)
+                <p>Ramdas</p>
+            ))} */}
+            <p>Ramdas</p>
+        </div>
+    )
+}
+
+export default List
