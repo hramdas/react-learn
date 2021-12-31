ca71a069e4a2ec1b6e279ed3815dc5b826ada21c
Ramdas Hedgapure <hedgapurer@gmail.com>
2021-12-31 21:55:38 +0530

    timer

--- 6-1.Timer/src/App.js
+++ 6-1.Timer/src/App.js
@@ -3,18 +3,32 @@ import Timer from './components/Timer'
 import {useState} from 'react'
 
 function App() {
-  const [showtime, setShowtime] = useState(true)
-  const [start, setStart] = useState(0)
-  const end = 3
+  const [showtime, setShowtime] = useState(false)
+  const [time, setTime] = useState({})
+  // const [end, setEnd] = useState(null)
+
+  const handleChange = (e)=>{
+    let {name, value} = e.target
+  
+    setTime({...time, [name] : value})
+  }
+  const handleClick = (e)=>{
+    e.preventDefault()
+    console.log()
+    setShowtime(!showtime)
+  }
 
  return(
    <div className='App'>
-      {/* < Timer initial={start} end={end}  /> */}
 
-      { showtime ? < Timer initial={start} end={end}  /> : null}
+      { showtime ? < Timer data={time}  /> : null}
 
-      <button onClick={ ()=>setShowtime(!showtime)} >{showtime ? "Hide" : "Show"}</button>
-      
+      <input  type="number" name="start" placeholder='Start value' onChange={handleChange}  />
+      <input type="number" name="end" placeholder='Final value' onChange={handleChange} />
+
+      <button onClick={ handleClick} >{showtime ? "Hide" : "Show"}</button>
+
+
    </div>
  )
 }
--- 6-1.Timer/src/components/Timer.jsx
+++ 6-1.Timer/src/components/Timer.jsx
@@ -1,26 +1,45 @@
 import { useEffect, useState } from "react";
 
-function Timer(props){
-    let [time, setTime] = useState(props.initial)
+
+
+function Timer({data}){
+    const start = +data.start
+    const end = +data.end
+    let [time, setTime] = useState(start)
 
     useEffect(()=>{
-        const id = setInterval(()=>{
+        let id = setInterval(()=>{
             setTime((p)=>{
-                if(p === props.end){
+                if(p === end){
                     clearInterval(id)
-                    return props.end
+                    return end
                 }
                 return p+1
             })
         }, 1000)
 
+        //To stop memory leak
+        return ()=>{
+            // console.log('unmounted')
+            clearInterval(id)
+        };
+       
     }, [])
+
+    const handleStop = (id)=>{
+        clearInterval(id)
+        setTime((p)=>p
+        )
+    }
     
     return(
         <>
             <h1>Timer : {time}</h1>
+
+            {/* <button onClick={handleStop}>Stop</button> */}
+
+            <button onClick={()=>setTime(start)}>Restart</button>
         </>
     )
 }
-
 export default Timer
