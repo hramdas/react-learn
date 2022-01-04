9931faa8ec0917d750f38d854db22cac3dc9d539
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-01-04 16:50:35 +0530

    Context API

---
+++ 8.contextAPI/src/components/Cart.jsx
@@ -0,0 +1,9 @@
+import { useContext } from "react"
+import { CartContext } from "../contexts/CartContext"
+
+export const Cart = ()=>{
+    const {cart} = useContext(CartContext)
+    return(
+        <div>Cart {cart}</div>
+    )
+}
---
+++ 8.contextAPI/src/contexts/CartContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext();
+
+export const CartContextProvider = ({children}) =>{
+
+    const [cart, setCart] = useState(0)
+    const handleCartUpdate = (value) =>{
+        setCart(cart + value)
+    }
+    //context provider
+    return <CartContext.Provider value={{cart, handleCartUpdate }}>
+        {children}
+    </CartContext.Provider>
+}
