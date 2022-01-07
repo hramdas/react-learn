d0a356fca134fdad0956a4e26dd4c0ac6b1563c4
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-01-07 16:33:47 +0530

    redux

---
+++ 11.redux-intro/store.js
@@ -0,0 +1,43 @@
+// const action = {type : "",  payload : ""};  //Fixed   //payload optional
+// const decCountAction = {type : "DEC_COUNT",  payload : 1};
+
+const {createStore} = require('redux')
+
+
+// class Store {
+//     constructor(reducer, init){
+//         this.reducer = reducer;
+//         this.state = init
+//     }
+//     getState(){
+//         return this.state
+//     }
+//     dispatch(action){   //calls reducer
+//         this.state = this.reducer(this.state, action);
+//     };
+// }
+
+
+//pure function 
+//Fixed
+const reducer = (state, {type, payload}) =>{   //check type and decide command
+    switch(type){
+        case "INC_COUNT" :
+            return {...state, count : state.count + payload};
+        case "DEC_COUNT" :
+            return {...state, count : state.count - payload};
+
+        default :
+            return state;
+    }
+};
+
+const init = { count : 1}
+
+const store = new createStore(reducer, init);  // Fixed
+console.log(store.getState())
+
+store.dispatch({type : "INC_COUNT", payload : 1})
+
+console.log(store.getState())
+
