3a9653456d5a7f4e9790f14a9ec452f0cc5204de
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-01-17 00:45:43 +0530

    react-redux 11/01/2022

--- 12-1-react-redux/src/components/TodoItem.jsx
+++ 12-1-react-redux/src/components/TodoItem.jsx
@@ -1,15 +1,17 @@
-import { useParams } from "react-router-dom"
+import { useParams, Navigate, Link } from "react-router-dom"
 import { useEffect, useState } from "react"
 
 export const TodoItem = ()=>{
     const {id} = useParams()
     const [todo, setTodo] = useState({})
+    const [show, setShow] = useState(false)
+    const [edittext, setEdittext] = useState()
 
     useEffect(() => {
         fetch(`http://localhost:3001/todos/${id}`)
         .then(d=>d.json())
         .then((res)=>setTodo(res))
-    }, []);
+    }, [show]);
 
     const handleUpdate = (s)=>{
         fetch(`http://localhost:3001/todos/${id}`,{
@@ -22,16 +24,62 @@ export const TodoItem = ()=>{
         .then(d=>d.json())
         .then(res=>setTodo(res))
     }
-    
+
+    const handleRemove = (id)=>{
+        fetch(`http://localhost:3001/todos/${id}`,{
+            method : "DELETE",
+            headers:{
+                "Content-Type" : "application/json"
+            }
+        })
+        .then(d=>d.json())
+        .then(res=>{
+            // navgate()
+        })
+    }
+    const showEdit = (text)=>{
+        setShow(true)
+        setEdittext(text)
+    }
+    const handleEdit = (e)=>{
+        setEdittext(e.target.value)
+    }
+    const saveEdit = (i) =>{
+        fetch(`http://localhost:3001/todos/${id}`,{
+            method : "PATCH",
+            headers:{
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify({ title : i })
+        })
+        .then(d=>d.json())
+        .then((res)=>{
+            setShow(false)
+        })
+    }
+
     return(
         <div>
             Todo Item {id}
-
             <h3>{todo.title}</h3>
-            <span>{todo.status ? "Done" :" Not Done"} <button onClick={()=>handleUpdate(!todo.status)}>Change</button> </span>
-            <br />
-            <br />
-            <button onClick={()=>handleUpdate()}>Edit</button>
+            <div>{todo.status ? "Done" :" Not Done"} <button onClick={()=>handleUpdate(!todo.status)}>Change</button> </div>
+
+            <span id="btnspan">
+                <button onClick={()=>showEdit(todo.title)}>Edit</button>
+                <Link to="/">
+                <button onClick={()=>handleRemove(todo.id)}>Remove</button>
+                </Link>
+            </span>
+
+            {show?
+             <div>
+                 <textarea value={edittext} onChange={handleEdit} />
+                 <button onClick={()=>saveEdit(edittext)}>Save</button>
+             </div> : <span></span>
+             }
+             <br />
+              <Link to="/">Go toHome</Link>
+           
         </div>
     )
 }
