30d19c717c5bdaf0af6890f94595dbb937b94e22
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-01-17 17:45:24 +0530

    final

--- eval2/src/components/Navbar.jsx
+++ eval2/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom"
+import { AuthContext } from "./context/AuthContext";
+import { useContext } from "react";
 
 export const Navbar = () => {
+    const {user} = useContext(AuthContext)
     return (
         <div className="navbar">
             <Link id="navitem" to="/">Home</Link>
             <Link id="navitem" to="/login">Login</Link>
             <Link to="/post">Add new job</Link>
-
         </div>
     )
 }
